d9ed83525eb86e263f040d1df76930ac4db00c81
moncapiten <moncapiten@gmail.com>
2025-02-27 12:19:49 +0100

    done the new measuring script and new data from sim taken

--- Code/matlab/dataVisual.m
+++ Code/matlab/dataVisual.m
@@ -2,26 +2,35 @@ clear all;
 
 % Data( media) position and name, to retrieve( save) files from( to) the correct position
 dataPosition = '../../Data/';
-filename = ['dataForXYPlot'];
+filename = 'opbapa';
 %filename = 'AD8031';
 
 %mediaposition = '../../Media/';
 %medianame = strcat('bodeRLC-simulation-Qx2');
 
 % data import and conditioning
-rawData = readmatrix(strcat(dataPosition, filename, '.txt'));
+rawData = readmatrix(strcat(dataPosition, filename, '.txt'))
 
 tt = rawData(:, 1);
 ch1 = rawData(:, 2);
 ch2 = rawData(:, 3);
 
 % plotting
+
+t = tiledlayout(2, 1);
+ax1 = nexttile;
 plot(tt, ch1, 'o')
 hold on
 plot(tt, ch2)
 hold off
 
+ax2 = nexttile;
+axs = [ax1, ax2];
+plot(ch2, ch1);
 
+
+grid(ax1, 'on');
+grid(ax1, 'minor');
 
 
 plot(ch1, ch2);
--- Code/matlab/nonLinearDistortions.m
+++ Code/matlab/nonLinearDistortions.m
@@ -5,38 +5,59 @@ gamma = 1;   %prova 1, 0.2 e 4(osserva grafico sx e vedi sottoperiodi e periodi 
 k1 = 10;
 k3 = [0, 10, 80];
 %k3 = [0, 2, 10];
-A = 0.05;
-f0 = 1;
+A = 1;
+f0 = 100;
 omega = 2*pi*f0;
 
 t0 = 0;
-durata = 20;
-x0 = -1.2; %try -1.4
+durata = 15;
+x0 = -0.5; %try -1.4
 v0 = 0;
 
 times = linspace(t0, durata, 1e5);
 
 
+t = tiledlayout(2,2, "TileSpacing", "tight", "Padding", "compact");
+ax1 = nexttile([1, 1]);
+ax3 = nexttile([2, 1]);
+ax2 = nexttile([1, 1]);
 
-for i = 1:3
-    ff = @(t, xv)[xv(2); -gamma*xv(2) - (k1/m)*xv(1) - (k3(i)/m)*xv(1)^3 + (A/m)*sin(omega * t)];
+axs = [ax1, ax2, ax3];
+
+for i = 2:2
+
+    ff = @(t, xv)[xv(2); -gamma*xv(2) - (k1/m)*xv(1) - (k3(2)/m)*xv(1)^3 + (A/m)*sin(omega * t)];
     [tt xv] = ode45(ff,times,[x0 v0]);
-    xx = xv(:,1);
-    vv = xv(:,2);
-    plot(xx, vv);
+
+    plot(ax1, tt, xv(:,1));
+    plot(ax2, tt, xv(:,2));
+    plot(ax3, xv(:,1), xv(:,2));
+
     if i == 1
-        hold on
+        hold(ax1, "on");
+        hold(ax2, "on");
+        hold(ax3, "on");
     end
-    i
 end
 
-legendentries = compose('k3 = %d', k3);
-legend(legendentries)
 
-grid on;
-grid minor;
+title(t, ['NLD - Super-Period - f = ', num2str(f0, '%.2f'), ' Hz'], 'Interpreter', 'latex', 'FontSize', 18);
+%title(t, 'NLD - Super-Period - f = ${f0}$', 'Interpreter', 'latex')
 
-xlabel('x [u.a.]');
-ylabel('v [u.a.]');
+grid(axs, "on");
+grid(axs, "minor");
 
-title('Distorsioni non lineari - k1 = 10');
+ylabel(ax1, 'x [u.a.]', 'Interpreter', 'latex', 'FontSize', 14);
+xlabel(ax1, 't [s]', 'Interpreter', 'latex', 'FontSize', 14);
+
+ylabel(ax2, 'v [u.a.]', 'Interpreter', 'latex', 'FontSize', 14);
+xlabel(ax2, 't [s]', 'Interpreter', 'latex', 'FontSize', 14);
+
+ylabel(ax3, 'v [u.a.]', 'Interpreter', 'latex', 'FontSize', 14);
+xlabel(ax3, 'x [u.a.]', 'Interpreter', 'latex', 'FontSize', 14);
+
+
+linkaxes([ax1, ax2], 'x');
+%linkaxes([ax1, ax2], 'y');
+
+%ylim(ax1, [-1, 1]);
